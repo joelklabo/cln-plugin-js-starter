62e452602658bd4c21f1ac130b03280caa394376
Joel Klabo <joelklabo@gmail.com>
2023-04-17 18:50:15 -0700

    Update to use methods

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 import { Plugin } from "./plugin.js";
-import { log } from "./log.js";
 
-const plugin = new Plugin()
+const plugin = new Plugin({ dynamic: true })
+
+plugin.addOption("test", "string", "test option", "test")
+plugin.addMethod("testinfo", "get info", "")
 
 plugin.start()
